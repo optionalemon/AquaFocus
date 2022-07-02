f50bf0ab83421c3d9709eee85ec3c8e37cdbda33
Yan Xiaoying <83106320+xiaoying1129@users.noreply.github.com>
2022-07-02 17:51:47 +0800

    Add files via upload

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -2,6 +2,9 @@ class CreateToDo {
   String? uid;
   String? title;
   bool? isComplete;
+  //DateTime? deadline;
 
-  CreateToDo({this.uid, this.title, this.isComplete});
+  CreateToDo({this.uid, this.title, this.isComplete,
+    //this.deadline
+  });
 }
