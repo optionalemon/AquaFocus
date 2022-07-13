257edf5305b8a10abbccbb5900cbfedf9f4781b7
xiaoying <xiaoying1129@gmail.com>
2022-07-14 04:08:01 +0800

    added focus timer backend and statistics

---
+++ lib/screens/UserPages/Statistics/bar_chart.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class LineChartWidget extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) => LineChart(
+    LineChartData(
+      
+    )
+  );
+}
