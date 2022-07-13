e4665840d58238e954faf7efd1ca08e2f1ed25a7
Yan Xiaoying <83106320+xiaoying1129@users.noreply.github.com>
2022-07-13 12:11:54 +0800

    Add files via upload

--- lib/screens/UserPages/aquarium_screen.dart
+++ lib/screens/UserPages/aquarium_screen.dart
@@ -1,4 +1,8 @@
+import 'package:AquaFocus/model/marine_creature.dart';
 import 'package:flutter/material.dart';
+import 'package:AquaFocus/services/database_services.dart';
+import 'package:AquaFocus/loading.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AquariumScreen extends StatefulWidget {
   @override
@@ -6,23 +10,100 @@ class AquariumScreen extends StatefulWidget {
 }
 
 class _AquariumScreenState extends State<AquariumScreen> {
+
+  bool loading = true;
+  List marLives = [];
+  List<String> aqList = [];
+
+  Future<void> getMarLives() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      marLives = await DatabaseService().getMarLivesList(user.uid);
+      for (int i = 0; i < marLives.length; i++) {
+        for (int j = 0; j < 6; j++) {
+          if (marLives[i] == j) {
+            aqList.add(marinesCreatures[j].image);
+          }
+        }
+      }
+    }
+    setState(() {
+      loading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    getMarLives();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        title: const Text("Aquarium"),
-        backgroundColor: Color.fromARGB(40, 0, 0, 0),
-      ),
-      body: Container(
-          constraints: BoxConstraints.expand(),
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/background2.png'),
-              fit: BoxFit.cover,
+    Size size = MediaQuery.of(context).size;
+    return loading
+        ? const Loading()
+        : Scaffold(
+        resizeToAvoidBottomInset: false,
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: const Text("Aquarium"),
+          backgroundColor: Color.fromARGB(40, 0, 0, 0),
+        ),
+        body: Stack(
+            children: <Widget>[
+              Container(
+                constraints: BoxConstraints.expand(),
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/background2.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SafeArea(
+                  child: buildAqCard(aqList),
+              )
+            ]
+        )
+    );
+  }
+
+  buildAqCard(List<String> aqList) {
+    Size size = MediaQuery.of(context).size;
+    return ListWheelScrollView(itemExtent: 250,
+        children: aqList.map((e) =>
+            SizedBox(
+          width: size.width*0.8,
+          child: Container(
+            padding: EdgeInsets.all(size.width * 0.04),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.white.withOpacity(0.4),
+                      Colors.white.withOpacity(0)
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  boxShadow: const <BoxShadow>[
+                    BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 5,
+                        offset: Offset(0.0, 5)
+                    )
+                  ]
+              ),
+            child: SizedBox(
+                child: Image.asset(e)
             ),
-          ),)
+          )
+            ),
+        )
+            .toList()
     );
   }
 }
+
