beb1fa49dd7b916afb02ffdd97da2ef54e6aaf10
Yan Xiaoying <83106320+xiaoying1129@users.noreply.github.com>
2022-07-17 21:25:52 +0800

    Add files via upload

--- lib/services/database_services.dart
+++ lib/services/database_services.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'package:AquaFocus/screens/FocusTimer/CountDown/countdown_helper.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:AquaFocus/model/app_user.dart';
 import 'package:AquaFocus/model/tags.dart';
@@ -40,22 +41,22 @@ class DatabaseServices {
   Future<List> getUserTags() async {
     List tags = [];
     await tagsCollection.get().then((snapshot) => {
-          snapshot.docs.forEach((doc) {
-            String color = doc.get("color");
-            String title = doc.get("title");
+      snapshot.docs.forEach((doc) {
+        String color = doc.get("color");
+        String title = doc.get("title");
 
-            tags.add(Tags(title: title, color: color));
-          })
-        });
+        tags.add(Tags(title: title, color: color));
+      })
+    });
     return tags;
   }
-  
+
   Future<double> getPercentageForTag(Tags tag) async {
     QuerySnapshot tasks = await userDoc.collection('tasks').where('tag', isEqualTo: tag.title).get();
     //print(tasks.size);
     QuerySnapshot totalTasks = await userDoc.collection('tasks').get();
     //print(totalTasks.size);
-    return double.parse((tasks.size / totalTasks.size * 100).toStringAsFixed(2));
+    return tasks.size / totalTasks.size * 100;
   }
 
   Future<void> addUser(AppUser user, String uid) async {
@@ -176,18 +177,109 @@ class DatabaseServices {
     doc.update({"marlives": marList});
   }
 
-  Future<void> saveFocusTime(int duration, String date) async {
+  Future<List<int>> getListForHours(String databaseField, String userId) async {
+    List<int> requestList = [];
+    DocumentReference docRef = userCollection
+        .doc(userId)
+        .collection('FocusTime')
+        .doc(DateFormat('yyyy-MM-dd')
+        .format(DateTime.now()));
+
+    await docRef.get().then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        var requestListRaw = [];
+        try {
+          requestListRaw = documentSnapshot.get(databaseField);
+        } catch (StateError) {
+          print("rq list does not exist");
+        }
+
+        requestList = List<int>.from(requestListRaw);
+      }
+    });
+
+    return requestList;
+  }
+
+  Future<List<int>> getHoursList(String userId) {
+    return getListForHours('Hours', userId);
+  }
+
+  Future<void> saveFocusTime(int duration, String date, DateTime startTime, DateTime endTime) async {
     var focusTimeCollection = userDoc.collection('FocusTime');
+
+    Duration diff = endTime.difference(startTime);
+    List totalTime = CountDownHelper().timeString(diff.inSeconds);
+    int diffInt = int.parse(totalTime[2]) +
+        int.parse(totalTime[1]) * 60 +
+        int.parse(totalTime[0]) * 60;
+    var hours = await getHoursList(userDoc.id);
+
+
+    if (hours.isEmpty) {
+      hours = List.generate(24, (index) => 0);
+      for(int i = 0; i < 24; i++) {
+        if(i == startTime.hour && i == endTime.hour) {
+          hours[i] = hours[i] + diffInt;
+        }
+        else if (i == startTime.hour && i != endTime.hour) {
+          Duration diffToStart = DateTime(
+              DateTime.now().year,
+              DateTime.now().month,
+              DateTime.now().day,
+              i + 1,
+              0
+          ).difference(startTime);
+          List time = CountDownHelper().timeString(diffToStart.inSeconds);
+          int diffToStartInt = int.parse(time[2]) +
+              int.parse(time[1]) * 60 +
+              int.parse(time[0]) * 60;
+
+          hours[i] = hours[i] + diffToStartInt;
+
+          for(int j = 0; j < 24; j++) {
+            if (j == endTime.hour) {
+              Duration diffToEnd = endTime.difference(DateTime(
+                  DateTime.now().year,
+                  DateTime.now().month,
+                  DateTime.now().day,
+                  j,
+                  0
+              ));
+              List time2 = CountDownHelper().timeString(diffToEnd.inSeconds);
+              int diffToEndInt = int.parse(time2[2]) +
+                  int.parse(time2[1]) * 60 +
+                  int.parse(time2[0]) * 60;
+
+              hours[j] = hours[j] + diffToEndInt;
+              for (int k = i + 1; k < j; k++) {
+                hours[k] = hours[k] + 60;
+              }
+            }
+          }
+        }
+      }
+    } else {
+      for(int i = 0; i < 24; i++) {
+        if(i == startTime.hour && i == endTime.hour) {
+          hours[i] = hours[i] + diffInt;
+        }
+      }
+    }
 
     final focusDate = await focusTimeCollection.doc(date).get();
     if (focusDate.exists) {
       focusTimeCollection
           .doc(date)
-          .update({"totalTime": FieldValue.increment(duration)});
+          .update({
+        "totalTime": FieldValue.increment(duration),
+        "Hours": hours,
+      });
     } else {
       focusTimeCollection.doc(date).set({
         "totalTime": duration,
         "Date": DateFormat('yyyy-MM-dd').format(DateTime.now()),
+        "Hours": hours,
       });
     }
   }
@@ -196,7 +288,7 @@ class DatabaseServices {
     num totalMinutes = 0;
 
     CollectionReference userFocusTime =
-        userCollection.doc(uid).collection('FocusTime');
+    userCollection.doc(uid).collection('FocusTime');
 
     await userFocusTime.doc(day).get().then((DocumentSnapshot doc) {
       if (doc.exists) {
@@ -204,6 +296,20 @@ class DatabaseServices {
       }
     });
     return totalMinutes;
+  }
+
+  Future<List> getTimeOfTheHours(String uid, String day) async {
+    List timeByHour = [];
+
+    CollectionReference userFocusTime =
+    userCollection.doc(uid).collection('FocusTime');
+
+    await userFocusTime.doc(day).get().then((DocumentSnapshot doc) {
+      if (doc.exists) {
+        timeByHour = doc.get("Hours");
+      }
+    });
+    return timeByHour;
   }
 
   Future<Map<DateTime, int>> heatMapData() async {
